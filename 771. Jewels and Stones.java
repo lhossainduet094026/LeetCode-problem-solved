7e2a758dd8a02109709f5690b1d01ec5b149185a
lhossainduet094026 <56044580+lhossainduet094026@users.noreply.github.com>
2020-02-28 12:47:38 +0600

    Add files via upload

---
+++ 771. Jewels and Stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+       int count = 0;
+        for (int i = 0; i < J.length(); i++) {
+            for (int k = 0; k < S.length(); k++) {
+                if (J.charAt(i) == S.charAt(k)) {
+                    count++;
+                }
+            }
+        } 
+        return count;
+    }
+}
